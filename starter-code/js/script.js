20447b2412289a64a35c7b80d30e7cde3c6b99c5
rglepe <rglepe@gmail.com>
2020-03-16 12:06:41 +0100

    init

---
+++ starter-code/js/script.js
@@ -0,0 +1,42 @@
+const deleteItem = (e) =>{
+
+}
+
+const getPriceByProduct = (itemNode) =>{
+
+}
+
+const updatePriceByProduct = (productPrice, index) =>{
+
+}
+
+const getTotalPrice = ()  =>{
+
+}
+
+const createQuantityInput = () =>{
+
+}
+
+const createDeleteButton = () =>{
+
+}
+
+const createQuantityNode = () =>{
+
+}
+
+const createItemNode = (dataType, itemData) =>{
+
+}
+
+const createNewItemRow = (itemName, itemUnitPrice) =>{
+
+}
+
+const createNewItem = () =>{
+
+}
+
+addEventListener = ('load',  ()=>{
+});
