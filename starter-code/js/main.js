80b9f47a7c442929a35edd8567b610e625e0bf27
victorgg1993 <vigomga1993@gmail.com>
2020-10-23 20:19:07 +0200

    añadidas las funciones para pintar los article

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,6 +1,10 @@
 
 
 // main()
+pintar_article('Bloque vendedor', 'ul_productos_stock');
+pintar_article('Bloque Cliente', 'ul_productos_tienda');
+pintar_article('Bloque Ranking', 'ul_productos_ranking');
+
 print_productos_stock(objeto_JSON); // temporal
 print_productos_tienda(); // temporal
 print_productos_ranking(); // temporal
