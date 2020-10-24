afd033a35e10e518838899ec9cf13d86161d1022
victorgg1993 <vigomga1993@gmail.com>
2020-10-24 18:45:25 +0200

    se arregló la actualización de la cantidad / precio total

--- starter-code/js/vendedor.js
+++ starter-code/js/vendedor.js
@@ -9,8 +9,7 @@ function print_productos_stock(arr_productos_stock) {
         let datos = { // con esta estructura de datos solamente pasamos un parámetro, es más flexible
             padre: padre,
             elemento: arr_productos_stock[i],
-            id_input: i,
-            id_boton: i,
+            id: i,
             texto_btn: "Eliminar",
             lstn_in: listener_input_cantidad,
             lstn_btn: listener_boton_eliminar
@@ -24,6 +23,7 @@ function print_productos_stock(arr_productos_stock) {
         padre: padre,
         elemento: { name: "nombre producto", price: 0, quantity: 0 },
         texto_btn: "Añadir",
+        id_p: "add",
         lstn_in: listener_input_cantidad_add,
         lstn_btn: listener_boton_add
     };
@@ -38,6 +38,7 @@ function pintar_li_producto_stock(datos) {
 
     // <li>
     let lista = document.createElement('li');
+    lista.id = datos.id;
     datos.padre.appendChild(lista);
 
     switch (datos.texto_btn) { // Nombre producto
@@ -65,14 +66,19 @@ function pintar_li_producto_stock(datos) {
 
         case 'Eliminar': // Si es eliminar, el precio no es editable
             let precio = document.createElement('p');
-            precio.innerText = `precio ${datos.elemento.price}€`;
+            precio.id = ("p_precio_" + datos.id);
+            precio.innerText = `Precio: ${datos.elemento.price}€`;
             lista.appendChild(precio);
             break;
     }
 
+    let nombre_cantidad = document.createElement('label');
+    nombre_cantidad.innerText = `Cantidad:`;
+    lista.appendChild(nombre_cantidad);
+
     // casilla (editable) de cantidad
     let casilla_cantidad = document.createElement('input');
-    casilla_cantidad.id = datos.id_input;
+    casilla_cantidad.id = ("input_cantidad_" + datos.id);
     casilla_cantidad.placeholder = "Cantidad";
 
     if (datos.texto_btn == 'Eliminar') {
@@ -84,19 +90,20 @@ function pintar_li_producto_stock(datos) {
 
     // casilla precio final
     let precio_total = document.createElement('p');
-    precio_total.innerText = ` ${(datos.elemento.price * datos.elemento.quantity).toFixed(2)}€`;
+    precio_total.innerText = `Total: ${(datos.elemento.price * datos.elemento.quantity).toFixed(2)}€`;
+    precio_total.id = ("p_total_" + datos.id);
     lista.appendChild(precio_total);
 
     // botón eliminar
     let boton_eliminar = document.createElement('button');
-    boton_eliminar.id = datos.id_boton;
+    boton_eliminar.id = ("button_eliminar_" + datos.id);
     boton_eliminar.innerText = datos.texto_btn;
     lista.appendChild(boton_eliminar);
     boton_eliminar.addEventListener('click', datos.lstn_btn);
 }
 
 // No usar
-function listener_input_cantidad_add() {} // No usar
+function listener_input_cantidad_add() { } // No usar
 
 // To - do
 function listener_boton_add() {
@@ -105,6 +112,7 @@ function listener_boton_add() {
 
 // To - do
 function listener_boton_eliminar(evento_boton) {
+    console.log("debug: btn eliminar");
     let id_boton = evento_boton.explicitOriginalTarget.id;
     objeto_JSON.splice(id_boton, 1);
     /* Funciona solo la primera vez porque luego el ID del boton no coincide con el indice del producto en la array
@@ -115,21 +123,20 @@ function listener_boton_eliminar(evento_boton) {
 
 // To - do
 function listener_input_cantidad(evento_input) {
-    console.log("input listener");
-    let id_input = evento_input.explicitOriginalTarget.id;
-    objeto_JSON[id_input].quantity = parseInt(evento_input.explicitOriginalTarget.value);
-    /*
-        let padre = document.getElementById('ul_productos_stock');
-        let datos = { // con esta estructura de datos solamente pasamos un parámetro, es más flexible
-            padre: padre,
-            elemento: { name: "nombre producto", price: 0, quantity: 0 },
-            texto_btn: "Añadir",
-            lstn_in: listener_input_cantidad_add,
-            lstn_btn: listener_boton_add
-        };
 
-        pintar_li_producto_stock(datos);
-    */
+    let id_input = evento_input.explicitOriginalTarget.id;
+    id_input = id_input.replace(/^\D+/g, '');
+    objeto_JSON[id_input].quantity = parseInt(evento_input.explicitOriginalTarget.value);
+
+    // pintar de nuevo
+    let objeto_precio = document.getElementById(`p_precio_${id_input}`);
+    let objeto_input = document.getElementById(`input_cantidad_${id_input}`);
+    let asd = objeto_precio.innerText;
+
+    asd = objeto_precio.innerText.replace('€', '').replace(/^\D+/g, '');
+
+    let precio_nuevo = (asd * objeto_input.value);
+    document.getElementById(`p_total_${id_input}`).innerText = `Total: ${precio_nuevo.toFixed(2)}€`
 }
 
 // To - do
