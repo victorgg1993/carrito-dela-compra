73de415f99df8fd1e2829d261236d9ccef41a0a8
victorgg1993 <vigomga1993@gmail.com>
2020-10-25 18:21:04 +0100

    trabajando en los estilos del cliente

--- starter-code/js/cliente.js
+++ starter-code/js/cliente.js
@@ -1,22 +1,18 @@
 // To - do
 function print_productos_tienda(arr_productos_tienda) {
 
-    // borrar cuando se trabaje en ella, esto es sólo un ejemplo.
-    // Creas un botón en la lista de los productos de la tienda:
-
     for (let i = 0; i < arr_productos_tienda.length; i++) {
+
         let contenido_list =
             `<h2>${arr_productos_tienda[i].name}</h2>
     <img src="" alt="imagen producto">
-    <input placeholder="cantidad"></input>
-    <p>Unidades: ${arr_productos_tienda[i].quantity}</p>
-    <p>precio: ${arr_productos_tienda[i].price}€</p>
+    <input type="number" value="1" min="1" max="${arr_productos_tienda[i].quantity}"></input>
+    <p>${arr_productos_tienda[i].quantity} uds. disponibles</p>
+    <p id="p_precio_producto">precio: ${arr_productos_tienda[i].price}€</p>
     <button>Comprar</button>`;
 
         print_elemento('ul_productos_tienda', 'li', contenido_list);
     }
-
-
 }
 
 // To - do
