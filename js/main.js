f40d387a4ea546f76b656a20ac47ad96457fef72
victorgg1993 <vigomga1993@gmail.com>
2020-10-26 17:28:05 +0100

    Se adaptó el código para ser ejecutado desde github pages

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+// main()
+pintar_article('Bloque vendedor', 'ul_productos_stock', 'vendedor');
+pintar_article('Bloque Cliente', 'ul_productos_tienda', 'comprador');
+pintar_article('Bloque Ranking', 'ul_productos_ranking', 'manager');
+
+print_productos_stock(objeto_JSON); // temporal
+print_productos_tienda(objeto_JSON); // temporal
